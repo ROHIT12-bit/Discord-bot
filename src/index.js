905aa70960dc9a015d88346b5a7521ddd0526a90
ROHIT12-bit <reddyrohit823@gmail.com>
2026-05-17 18:19:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-const express = require("express");
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+
+@app.route("/")
+def home():
+    return " bot is alive"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    Thread(target=run).start()
+      const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
