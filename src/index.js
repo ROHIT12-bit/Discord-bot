903315da7220dbb10db28cb3cdb7f7793d6bcd5c
ROHIT12-bit <reddyrohit823@gmail.com>
2026-05-17 08:11:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
 const Discord = require('discord.js');
 const chalk = require('chalk');
 require('dotenv').config('./.env');
